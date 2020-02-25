00b5ebff8adc467cf8bd64821dfff67d50a7a902
GAPUR EVLOEV <g.evloev@itfsystems.ru>
2020-02-25 12:47:50 +0300

    fix link

--- src/blocks/components/coquette/index.js
+++
@@ -1,7 +0,0 @@
-var mybutton = document.querySelector(".particles-button");
-mybutton.onclick = function() {
-	mybutton.disabled = true;
-	setTimeout(function() {
-		mybutton.disabled = false;
-	}, 1000);
-};
